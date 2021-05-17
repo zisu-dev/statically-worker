3b203f84da262461140c99ac9d2e6443aab01446
thezzisu <thezzisu@gmail.com>
2021-05-17 21:36:40 +0800

    refactor: update

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # statically-worker
+
+Proxy statically CDN using Cloudflare worker. Enhanced.
+
+Features:
+
+- Statically CDN, proxied (works in China)
+- Origin / Referer validation
+- Pre-defined resources using KV `/defined/${KEY}`
+- Proxy Gravatar (works in China) `/gravatar/${EMAIL}[/${SIZE}]`
--- src/handler.ts
+++ src/handler.ts
@@ -1,21 +1,23 @@
+import gravatar from 'gravatar'
+
 declare global {
   const KV: KVNamespace
+  const ORIGINS: string
 }
 
 export async function handleRequest(req: Request): Promise<Response> {
   return route(req)
 }
 
-const allowedOrigins = [
-  'zzisu.dev',
-  'zisu.dev',
-  'zzs1.cn',
-  'zhangzisu.cn',
-  'swarm-p.com',
-  'localhost',
-  'pages.dev',
-  'vercel.app'
-]
+const allowedOrigins = ORIGINS.split(',').map((x) => x.trim())
+
+function NotFound() {
+  return new Response('Not found', { status: 404 })
+}
+
+function BadRequest() {
+  return new Response('Bad Request', { status: 400 })
+}
 
 function isValidOrigin(origin: string) {
   return allowedOrigins.some((x) => origin.endsWith(x))
@@ -26,7 +28,7 @@ function isValidReferer(ref: string) {
   return isValidOrigin(url.hostname)
 }
 
-async function followRedirect(req: Request): Promise<Response> {
+async function fFetch(req: Request): Promise<Response> {
   const res = await fetch(req, {
     headers: {
       Referer: ''
@@ -34,7 +36,7 @@ async function followRedirect(req: Request): Promise<Response> {
   })
   const location = res.headers.get('location')
   if ([301, 302, 303, 307, 308].includes(res.status) && location) {
-    return followRedirect(new Request(location))
+    return fFetch(new Request(location))
   } else {
     return res
   }
@@ -48,17 +50,13 @@ async function route(req: Request): Promise<Response> {
     return Response.redirect('https://' + main)
   }
 
-  function notFound() {
-    return new Response('Not found', { status: 404 })
-  }
-
   // Redirect / to main domain
   if (url.pathname === '/') return gotoMain()
 
   // Use icon from main domain
   if (url.pathname === '/favicon.ico') {
     url.hostname = main
-    return followRedirect(new Request(url.toString(), req))
+    return fFetch(new Request(url.toString(), req))
   }
 
   const referer = req.headers.get('Referer')
@@ -72,12 +70,22 @@ async function route(req: Request): Promise<Response> {
     if (!key) return gotoMain()
 
     const value = await KV.get(key)
-    if (value === null) return notFound()
+    if (value === null) return NotFound()
 
-    return followRedirect(new Request(value, req))
+    return fFetch(new Request(value, req))
+  }
+
+  // Gravatar
+  if (url.pathname.startsWith('/gravatar/')) {
+    const result = url.pathname.substr(10).match(/^([^/]+)(?:\/([^/]*))?/)
+    if (!result) return BadRequest()
+    const email = result[1]
+    const size = result[2]
+    const avatarUrl = gravatar.url(email, { size }, true)
+    return fFetch(new Request(avatarUrl))
   }
 
   // Other request should be proxied
   url.hostname = 'cdn.statically.io'
-  return followRedirect(new Request(url.toString(), req))
+  return fFetch(new Request(url.toString(), req))
 }
